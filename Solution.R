43ad921b92fec69ff4f57b6675cff0d713feab30
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2026-02-09 07:00:31 +0000

    Auto commit on Mon Feb  9 07:00:31 AM UTC 2026

--- Solution.R
+++ Solution.R
@@ -10,20 +10,27 @@ for (pkg in packages) {
 }
 
 # Import the file homes.csv into the dataframe homes
-homes <-read.csv("homes.csv")
+# Your code here
+homes <- read.csv("homes.csv")
 
 # Display the column names
+# Your code here
 print(colnames(homes))
 
 # Display the number of instances
+# Your code here
 print(nrow(homes))
 
-# Display the columns Price, Bath, Bed, Year, and Status for instances for which School is Edison
+# Display the columns Price, Bath, Bed, Year, and Status for instances
+# for which School is Edison
+# Your code here
 print(homes[homes$School == "Edison", c("Price", "Bath", "Bed", "Year", "Status")])
 
-# Add the column PriceSqFt to the dataframe with the values of Price divided by Floor
+# Add the column PriceSqFt to the dataframe with the
+# values of Price divided by Floor
+# Your code here
 homes$PriceSqFt <- homes$Price / homes$Floor
 
 # Display the first six rows of the dataframe with the new column
+# Your code here
 print(head(homes))
-
