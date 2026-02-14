73d229312d5454b93808e849d24958c8860d7344
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2026-02-14 03:11:58 +0000

    Auto commit on Sat Feb 14 03:11:58 AM UTC 2026

--- Solution.R
+++ Solution.R
@@ -1,21 +1,22 @@
-# add needed packages here separated by commas
-packages <- c("tidyverse")
+# Functions from base and stats package used, 
+# no additional packages needed.
 
-# Install packages if not already installed
-for (pkg in packages) {
-  if (!requireNamespace(pkg, quietly = TRUE)) {
-    cat("Installing package:", pkg, "\n")
-    install.packages(pkg)
-  }
+# Input two IQs, making sure that IQ1 is less than IQ2
+inputs <- as.integer(x = readLines(con = "stdin", n = 2, warn=FALSE))
+
+IQ1 <- inputs[1]
+IQ2 <- inputs[2]
+
+if(IQ1 > IQ2){
+    print("IQ1 should be less than IQ2. Enter numbers again.")
+    quit(save = "no")
 }
 
-# Read in the file mtcars.csv
-cars <- read.csv("mtcars.csv")
+# Calculate the probability that a randomly selected person has an IQ less than or equal to IQ1.
+probLT <- pnorm(IQ1, mean=100, sd=15)
 
-# Find the mean of the column wt
-mean <- mean(cars$wt)# Your code here
+# Calculate the probability that a randomly selected person has an IQ between IQ1 and IQ2
+probBetw <- pnorm(IQ2, mean=100, sd=14) - pnorm(IQ1, mean=100, sd=15)
 
-# Find the median of the column wt
-median <- median(cars$wt)# Your code here
-
-print(paste0("mean = ", format(round(mean, 3)), ", median = ", format(round(median, 3))))
+print(paste0("The probability that a randomly selected person has an IQ less than or equal to ", format(round(IQ1, 3)), " is ", format(round(probLT, 3)), "."))
+print(paste0("The probability that a randomly selected person has an IQ between ", format(round(IQ1, 3)), " and ", format(round(IQ2, 3)), " is ", format(round(probBetw, 3)),  "."))
