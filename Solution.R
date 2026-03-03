2ab3fc4979bde39f588302d67a1e9e7041ad6ff4
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2026-03-03 01:33:39 +0000

    Auto commit on Tue Mar  3 01:33:39 AM UTC 2026

--- Solution.R
+++ Solution.R
@@ -1,21 +1,23 @@
 suppressPackageStartupMessages(library(tidyverse))
-hmeq <- read.csv("hmeq_sample.csv")
+
+# Read in hmeq_small.csv
+hmeq <- read.csv("hmeq_small.csv")
 
 hmeq$CLNO <- as.numeric(hmeq$CLNO)
 
 # Create a new data frame with the rows with missing values dropped
-hmeqDelete <- drop_na(hmeq)
+hmeqDelete <- na.omit(hmeq)
 
 # Calculate the means of CLNO and YOJ
 meanCLNO = mean(hmeq$CLNO, na.rm=TRUE)
 meanYOJ = mean(hmeq$YOJ, na.rm=TRUE)
 
 # Create a new data frame with the missing values of CLNO and YOJ filled in by the mean of the column
-hmeqReplace <- replace_na(hmeq, list(CLNO = meanCLNO, 
-                        YOJ = meanYOJ))
+hmeqReplace <- hmeq %>% mutate(CLNO=ifelse(is.na(CLNO), meanCLNO, CLNO), YOJ=ifelse(is.na(YOJ), meanYOJ, YOJ))
+  
                         
 # Print the summary for each new data frame
 print("Summary of hmeqDelete is ")
-summary(hmeqDelete)
+print(summary(hmeqDelete))
 print("Summary of hmeqReplace is ")
-summary(hmeqReplace)
+print(summary(hmeqReplace))
