6319c38e529b8cfae11e8f9466ff4a813f6458b7
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2026-03-03 03:12:40 +0000

    Auto commit on Tue Mar  3 03:12:40 AM UTC 2026

--- Solution.R
+++ Solution.R
@@ -18,21 +18,20 @@ theme_set(theme_gray(base_size=18))
 colors <- c("#1f77b4","#ff7f0e", "#2ca02c", "#d62728",
             "#9467bd","#8c564b", "#e377c2", "#7f7f7f",
             "#bcbd22", "#17becf")
-            
+
 # Load the data set mpg
-mpg <- read_csv("mpg.csv")# Your code here
+mpg <- read.csv("mpg.csv", stringsAsFactors = FALSE)
 
 # Print summary of data frame
-print(summary(mpg))# Your code here
+summary(mpg)
 
 # Create a scatter plot of weight vs mpg with origin represented by color
 #  x label should be "Weight" and y label should be "MPG"
-png(file="mpgScatterK.png")
+png(file = "mpgScatterC.png")
 
-p <- ggplot(mpg, aes(x = weight, y = mpg, color = factor(origin))) +
+p <- ggplot(mpg, aes(x = weight, y = mpg, color = origin)) +
   geom_point() +
-  labs( x = "Weight",
-        y = "MPG",
-        color = "Origin")
+  scale_color_manual(values = colors) +
+  labs(x = "Weight", y = "MPG")
 
-ggsave("mpgScatterK.png", plot=p, width=6, height=4, dpi=300)
+ggsave("mpgScatter.png", plot=p, width=6, height=4, dpi=300)
