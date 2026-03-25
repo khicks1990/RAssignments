ce9a3c59e52eadd7e5e9a8f2ef80438b89300f21
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2026-03-25 15:21:43 +0000

    Auto commit on Wed Mar 25 03:21:43 PM UTC 2026

--- Solution.R
+++ Solution.R
@@ -9,60 +9,23 @@ for (pkg in packages) {
   }
 }
 
-suppressPackageStartupMessages(library(tidyverse))
+# Read in nbaallelo_slr.csv
+nba <- read.csv("nbaallelo_slr.csv")
 
-# Increases font size for all ggplot2 plots
-theme_set(theme_gray(base_size=18))
+# Create a new column in the data frame that is the difference between pts and opp_pts
+nba$y <- nba$pts - nba$opp_pts
 
-# List of colors for customizing plots
-colors <- c("#1f77b4","#ff7f0e", "#2ca02c", "#d62728",
-            "#9467bd","#8c564b", "#e377c2", "#7f7f7f",
-            "#bcbd22", "#17becf")
-# Load titanic.csv
-titanic <- read.csv("titanic.csv")
+# Fit a least squares regression model on y and elo_i
+SLRModel <- lm(y ~ elo_i, data = nba)
 
-# Subset the titanic dataset to include first class passengers who embarked in Southampton
-firstSouth <- titanic |>
-  filter(pclass == 1, embarked == "S")
+# Print the intercept
+intercept <- coef(SLRModel)[1]
+print(paste0("The intercept of the linear regression line is ", format(round(intercept, 3)), "."))
 
-# Subset the titanic dataset to include either second or third class passenger
-secondThird <- titanic |>
-  filter(pclass == 2 | pclass == 3)
+# Print the slope
+slope <- coef(SLRModel)[2]
+print(paste0("The slope of the linear regression line is ", format(round(slope, 3)), "."))
 
-print(
-firstSouth |>
-  group_by(pclass, sex) |>
-  summarize(n=n(), .groups="drop_last") |>
-  spread(sex, n)
-)
-    
-print(
-secondThird |>
-  group_by(pclass, alive) |>
-  summarize(n=n(), .groups="drop_last") |>
-  spread(alive, n)
-)
-
-# Create a bar chart for the first class passengers who embarked in Southampton grouped by sex
-p <- ggplot(firstSouth, aes(x = factor(pclass), fill = sex)) +
-  geom_bar(position = "dodge") +
-  labs(x = "Class", y = "Count", fill = "Sex")
-
-ggsave("titanicBar1.png", plot = p, width = 6, height = 4, dpi = 300)
-
-# Create a bar chart for the second and third class passengers grouped by survival status
-p2 <- ggplot(secondThird, aes(x = factor(pclass), fill = alive)) +
-  geom_bar(position = "dodge") +
-  labs(x = "Class", y = "Count", fill = "Alive")
-
-ggsave("titanicBar2.png", plot = p2, width = 6, height = 4, dpi = 300)
-
-
-
-
-
-
-
-
-
-
+# Compute the proportion of variation explained by the linear regression
+rSquare <- summary(SLRModel)$r.squared
+print(paste0("The proportion of variation explained by the linear regression model is ", format(round(rSquare, 3)), "."))
