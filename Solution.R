c00ce61984feaf235f9d6987ec7177070c394a41
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2026-04-08 23:47:33 +0000

    Auto commit on Wed Apr  8 11:47:33 PM UTC 2026

--- Solution.R
+++ Solution.R
@@ -1,5 +1,5 @@
 # add needed packages here separated by commas
-packages <- c("tidyverse", "tidymodels", "kknn")
+packages <- c()
 
 # Install packages if not already installed
 for (pkg in packages) {
@@ -28,21 +28,26 @@ train_data <- training(data_split)
 test_data <- testing(data_split)
 
 # Define model with k=3
-skySurveyKNNClass <- nearest_neighbor(neighbors=3) %>% set_mode("classification") %>% set_engine("kknn")
+skySurveyKNNClass <- nearest_neighbor(mode = "classification", neighbors = 3) |>
+  set_engine("kknn")
 
 # Define recipe to normalize data
-skySurveyRecipe <- recipe(class ~ redshift + u_g, data = train_data) %>% step_normalize(all_predictors())
+skySurveyRecipe <- recipe(class ~ redshift + u_g,
+                          data = train_data) |>
+  step_normalize(all_numeric_predictors())
 
 # Assemble workflow
-skySurveyClassWflow <- workflow() %>% add_model(skySurveyKNNClass) %>% add_recipe(skySurveyRecipe)
+skySurveyClassWflow <- workflow() |>
+  add_recipe(skySurveyRecipe) |>
+  add_model(skySurveyKNNClass)
 
 # Fit model
-skySurveyClassFit <- fit(skySurveyClassWflow, data = train_data)
+skySurveyClassFit <- fit(skySurveyClassWflow, train_data)
 
 # Predict values for the test set and add those values onto to the test set.
-testPred <- predict(skySurveyClassFit, test_data) %>% bind_cols(test_data)
+testPred <- augment(skySurveyClassFit, test_data)
 
 # Print accuracy and confusion matrix
-testPred |> accuracy(truth= class,  estimate = .pred_class)
-confusionMatrix <- testPred |> conf_mat(truth = class, estimate = .pred_class) 
+testPred |> accuracy(class, .pred_class)
+confusionMatrix <- testPred |> conf_mat(class, .pred_class) 
 print(confusionMatrix)
