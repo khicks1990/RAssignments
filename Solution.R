f6288fff47cd309e864dfaf4ef17f91217fba9f6
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2026-05-03 23:33:30 +0000

    Auto commit on Sun May  3 11:33:30 PM UTC 2026

--- Solution.R
+++ Solution.R
@@ -18,27 +18,28 @@ heart <- read.csv("heart.csv")
 heart$target <- as.factor(heart$target)
 
 # Initialize the model with XGBoost and 50 trees
-# Your code here
-heartModel <- boost_tree(trees = 50) %>%
+heartBoost <- boost_tree(trees = 50) %>%
   set_engine("xgboost") %>%
-  set_mode("classification")
-
+  set_mode("classification")# Your code here
 
 # Fit the model
-# Your code here
-heartFit <- heartModel %>%
-  fit(target ~ age + cp + trestbps + chol + fbs + restecg + thalach + exang + oldpeak,
-      data = heart)
+heartFit <- heartBoost %>%
+  fit(
+    target ~ age + cp + trestbps + chol + fbs + restecg + thalach + exang + oldpeak,
+    data = heart
+  )# Your code here
 
 # Add predictions and class probabilities to heart dataset
-# Your code here
-heartPred <- predict(heartFit, new_data = heart)
+heartPred <- predict(heartFit, new_data = heart) 
+
 heartProb <- predict(heartFit, new_data = heart, type = "prob")
-heart <- bind_cols(heart, heartPred, heartProb)
 
-print(head(heart))
-
+heart <- bind_cols(heart, heartPred, heartProb) # Your code here
 
 # Confusion matrix using the caret package
-heartConf <- confusionMatrix(heart$.pred_class, heart$target)
-heartConf
+heartConf <- confusionMatrix(
+  data = heart$.pred_class,
+  reference = heart$target
+) # Your code here
+
+print(heartConf)
