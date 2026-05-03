815bf4e96306140bdc38d462c3f6b1036de6a822
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2026-05-03 05:20:27 +0000

    Auto commit on Sun May  3 05:20:27 AM UTC 2026

--- Solution.R
+++ Solution.R
@@ -1,5 +1,5 @@
 # add needed packages here separated by commas
-packages <- c()
+packages <- c("rpart", "ranger", "vip")
 
 # Install packages if not already installed
 for (pkg in packages) {
@@ -10,46 +10,45 @@ for (pkg in packages) {
 }
 
 suppressPackageStartupMessages(library(tidymodels))
+library(vip) 
 
 # Load the mpg dataset
+# Make sure mpg.csv is in your /workspaces/... folder
 mpg <- read.csv("mpg.csv")
 
 # Create a binary outcome variable for high mpg
-mpg <- ifelse(mpg$mpg > median(mpg$mpg), "High", "Low")
-mpg$high_mpg <- as.factor(mpg$high_mpg)
+mpg <- mpg %>%
+  mutate(high_mpg = factor(ifelse(mpg >= 25, "yes", "no")))
 
 # Subset the data for classification
-mpgClassification <- mpg[, c("high_mpg", "weight", "horsepower", "model_year")]
+mpgClassification <- mpg %>%
+  select(weight, horsepower, model_year, high_mpg)
 
 # Initialize a classification tree with max depth 4 and minimum 10 samples per leaf
 set.seed(14092022)
-mpgCT <- decision_tree(
-  mode = "classification",
-  tree_depth = 4,
-  min_n = 10
-) %>%
-  set_engine("rpart")
+mpgCT <- decision_tree(tree_depth = 4, min_n = 10) %>%
+  set_engine("rpart") %>%
+  set_mode("classification")
 
 # Fit the classification tree
 fitClassTree <- mpgCT %>%
   fit(high_mpg ~ ., data = mpgClassification)
 
 # Print classification tree
-fitClassTree
+print(fitClassTree)
 
 
 # Initialize a random forest classifier with 300 trees and 2 variables tried at each split
 set.seed(14092022)
-mpgRF <- rand_forest(
-  mode = "classification",
-  trees = 300,
-  mtry = 2
-) %>%
-  set_engine("ranger", importance = "impurity")
+mpgRF <- rand_forest(mtry = 2, trees = 300) %>%
+  set_engine("ranger", importance = "impurity") %>%
+  set_mode("classification")
 
 # Fit the random forest model
 fitRF <- mpgRF %>%
   fit(high_mpg ~ ., data = mpgClassification)
 
 # Display variable importance
-vip(fitRF$fit) 
+fitRF %>%
+  extract_fit_parsnip() %>%
+  vip()
